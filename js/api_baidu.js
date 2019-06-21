33f1951c2e29b053734f8d3ddf92ed1364acae3e
yuepengfei <663831709@qq.com>
2019-06-21 11:58:34 +0800

    first commit

---
+++ js/api_baidu.js
@@ -0,0 +1,7 @@
+(function () {
+    window.HOST_TYPE = "2";
+    window.BMap_loadScriptTime = (new Date).getTime();
+    document.write(
+        '<script type="text/javascript" src="/js/api.js"></script>'
+    );
+})();
